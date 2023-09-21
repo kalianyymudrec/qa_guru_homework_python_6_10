50c468ab4423a4b0ed6ee7ea46229ffeccb5863f
Danila Panov <danila.panov.02@mail.ru>
2023-09-21 19:52:28 +0600

    high-level-step-objects

--- test/test_student_registration_form.py
+++ test/test_student_registration_form.py
@@ -1,7 +1,7 @@
 import os
 
-from qa_guru_python_6_10.pages.registration_page import RegistrationPage
-from tests.conftest import RES_DIR
+from pages.registration_page import RegistrationPage
+from test.conftest import RES_DIR
 
 
 def test_fill_out_and_submit_form():
@@ -10,13 +10,13 @@ def test_fill_out_and_submit_form():
     registration_page.open()
 
     (
-    registration_page
+        registration_page
         .fill_first_name('Danila')
         .fill_last_name('Panov')
         .fill_email('danila.panov.02@mail.ru')
         .fill_gender('Male')
         .fill_mobile_number('0123456789')
-        .fill_date_of_birth('2002', '8', '18')
+        .fill_date_of_birth('2002', '08', '18')
         .fill_subjects('Maths', 'English')
         .fill_hobbies('Reading', 'Music')
         .upload_picture(os.path.join(RES_DIR, 'image.jpg'))
@@ -27,12 +27,12 @@ def test_fill_out_and_submit_form():
     registration_page.submit()
 
     registration_page.should_have_registered(
-        ('Student Name', 'Danila Panov),
+        ('Student Name', 'Danila Panov'),
         ('Student Email', 'danila.panov.02@mail.ru'),
         ('Gender', 'Male'),
         ('Mobile', '0123456789'),
         ('Date of Birth', '18 August,2002'),
-        ('Subjects', 'Maths, Physics'),
+        ('Subjects', 'Maths, English'),
         ('Hobbies', 'Reading, Music'),
         ('Picture', 'image.jpg'),
         ('Address', 'Omsk'),
